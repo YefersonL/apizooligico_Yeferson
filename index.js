03b3be158f81d5501fc63fa064626ec4ede177d4
MaikiSoft <maicolestebancastillolopez@gmail.com>
2025-09-11 20:31:12 -0500

    Se implenta la conexion con la bd mongo y se crea la clase animals.js con el esquema de tabla

--- index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const http = requiere('http');
-
-const server = http.createserver(function(req, res){
-    res.writehead(200, { 'content-type': 'text/plain' });
-    res.end('Hola Mundo');
-
-});
-server.listen(5000);
---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get('/saludo',(req, res) => {
+    res.send('!Hola Mundo!')
+})
+
+app.listen(port, () => {
+    console.log('La aplicación se esta ejecutando')
+})
